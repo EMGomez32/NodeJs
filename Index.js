f9444578e0c4215491a87b43a9822f1a112fb55a
Emiliano Gomez <gomez.emiliano.martin@gmail.com>
2023-05-15 17:06:11 -0300

    En el trabajo

--- Index.js
+++ Index.js
@@ -4,3 +4,22 @@ function sumar(a,b){
 }
 var n1=5, n2=6
 console.log('La suma de '+n1+'mas'+n2+' es: '+ sumar(n1,n2))
+console.log(__filename)
+console.log(__dirname)
+function printHola() {
+    console.log( "Hola, mundo!");
+   }
+   // Now call above function after 2 seconds
+   setTimeout(printHola, 2000);
+   function printHola() { console.log( "Hola, mundo!");
+}
+// Now call above function after 2 seconds
+var t = setTimeout(printHola, 2000);
+// Now clear the timer
+clearTimeout(t);   
+function printHola() {
+    console.log( "Hola, mundo!");
+   }
+   // Now call above function after 2 seconds
+   setInterval(printHola, 2000);
+   
