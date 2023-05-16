343300225acf57c7f57e5bb66d546066b82ebf12
EMGomez32 <gomez.emiliano.martin@gmail.com>
2023-05-15 23:31:33 -0300

    Laboratorio3

---
+++ Laboratorio3/Index.js
@@ -0,0 +1,9 @@
+//const operaciones = require("./Operaciones");
+import operaciones from "./Operaciones.js";
+const a = 5;
+const b = 3;
+
+console.log(`Suma: ${operaciones.suma(a, b)}`);
+console.log(`Resta: ${operaciones.resta(a, b)}`);
+console.log(`Multiplicación: ${operaciones.multiplicacion(a, b)}`);
+console.log(`División: ${operaciones.division(a, b)}`);
