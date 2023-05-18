ddaeecc2bbb41da161330a72c0e3b1308df799f8
EMGomez32 <gomez.emiliano.martin@gmail.com>
2023-05-17 22:18:51 -0300

    Empezando con typescript

---
+++ tyscrip/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("hola asda ");
+console.log("Adios Emiliano");
+console.log("Adios dafdsf");
---
+++ tyscrip/src/index.ts
@@ -0,0 +1,6 @@
+import { getHeapCodeStatistics } from "v8";
+
+console.log("hola asda ");
+console.log("Adios Emiliano");
+console.log("Adios dafdsf");
+
